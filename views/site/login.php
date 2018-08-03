ca71bfce8b27f9e8245bd97f9efdc0fd388d54d9
Cristian <criswxart@gmail.com>
2018-08-03 11:21:49 -0400

    Cambie el nombre en la pagina de login

--- views/site/login.php
+++ views/site/login.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Acá se hará ingreso al personal que corresponde a la empresa</p>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
